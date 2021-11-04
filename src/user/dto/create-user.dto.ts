f4954a0d1f51d6d5d5f7e49c4e9202b5608d3fbe
Sheva Athalla <shevaathalla@gmail.com>
2021-11-04 19:23:01 +0700

    validating column user and item

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,5 +1,16 @@
-export interface CreateUserDto {
+import { IsEmail, IsNotEmpty, IsString, Min } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
   fullname: string;
+
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @Min(8)
   password: string;
 }
--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,4 +1,10 @@
-export interface UpdateUserDto {
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+export class UpdateUserDto {
+  @IsString()
+  @IsNotEmpty()
   fullname: string;
+
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
 }
